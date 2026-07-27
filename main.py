e78fc5d28df43151d7a6d32998631e33d9dcfab7
Abeoanderson <abeoanderson@gmail.com>
2026-07-27 11:48:30 -0600

    peice movemnt and boundry checking fixed

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ class Game:
         self.rows = rows
         self.cols = cols
         self.score = 0
-        self.level = 0
+        self.level = 1
         self.grid = [[0 for _ in range(cols)] for _ in range(rows)] # list compreheni
         self.next = None
         self.end = False
@@ -105,7 +105,86 @@ class Game:
         self.figure = self.next
         self.next = Shape(5,0)
 
+    # check collisions returns a bool for collison yes or no
+    def collision(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.figure.image():
 
+                    block_row = self.figure.y + i
+                    block_col = self.figure.x + j
+
+                    if block_col < 0 or block_col >= self.cols:
+                        return True
+
+                    if block_row >= self.rows:
+                        return True
+
+                    if block_row >= 0 and self.grid[block_row][block_col] > 0:
+                        return True
+
+        return False
+    # remove row
+
+    # peices fall
+    def move_down(self):
+        self.figure.y += 1
+
+        if self.collision():
+            self.figure.y -= 1
+            return False
+
+        return True
+
+    # move left 
+    def move_left(self):
+        self.figure.x -= 1
+
+        if self.collision():
+            self.figure.x += 1
+            return False
+
+        return True
+
+    # move right 
+    def move_right(self):
+        self.figure.x += 1
+
+        if self.collision():
+            self.figure.x -= 1
+            return False
+
+        return True
+
+
+    # Rotate piece
+    def rotate(self):
+        old_rotation = self.figure.orientation
+        old_x = self.figure.x
+
+        self.figure.rotate()
+
+        # Try normal rotation
+        if not self.collision():
+            return
+
+        # Try moving right
+        self.figure.x += 1
+        if not self.collision():
+            return
+
+        # Try moving left
+        self.figure.x -= 2
+        if not self.collision():
+            return
+
+        # Rotation failed
+        self.figure.x = old_x
+        self.figure.orientation = old_rotation
+    # Hard drop (slam)
+    def slam(self):
+        while self.move_down():
+            pass
 
 
 
@@ -114,6 +193,9 @@ class Game:
 # Main game loop
 def main():
     tetris = Game(ROWS, COLS)
+    counter = 0
+    move = True
+
     run = True
     while run:
         SCREEN.fill(BG_COLOR)
@@ -122,6 +204,41 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    tetris.move_left()
+
+                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    tetris.move_right()
+
+                elif event.key == pygame.K_UP or event.key == pygame.K_w:
+                    tetris.rotate()
+
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    tetris.move_down()
+
+                elif event.key == pygame.K_SPACE:
+                    tetris.slam()
+        keys = pygame.key.get_pressed()
+        # hold down keys keeps them moving
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            tetris.move_left()
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            tetris.move_right()
+
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            tetris.move_down()
+        counter += 1
+        if counter >= 10000:
+            counter = 0
+
+        if move:
+            if counter %(FPS //(tetris.level)) == 0:
+                if not tetris.end:
+                    tetris.move_down()
 
         tetris.make_grid()
 
@@ -134,13 +251,15 @@ def main():
                     x = BLOCK * (tetris.figure.x + j)
                     y = BLOCK * (tetris.figure.y + i)
 
-                    SCREEN.blit(shape, (x, y))
+                    if x >= 0 and y >= 0:
+                        SCREEN.blit(shape, (x, y))
                     pygame.draw.rect(
                         SCREEN,
                         WHITE,
                         (x, y, BLOCK, BLOCK),
                         1
                     )
+        CLOCK.tick(FPS)
         pygame.display.update()
 
 if __name__ == "__main__":
