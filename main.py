2bfc8fce34ad1302a372bd09d8f4ff7f10b01f60
Abeoanderson <abeoanderson@gmail.com>
2026-07-27 10:50:12 -0600

    shape class created

--- main.py
+++ main.py
@@ -47,12 +47,37 @@ font2 = pygame.font.Font(
     "Assets/VT323-Regular.ttf",
     20
 )
+
 # shape class
+class Shape:
+    #global variables for types I, Z, S, L, J, T, O and coordinates
+    VERSIONS = {
+        'I': [[1, 5, 9, 13], [4, 5, 6, 7]],
+        'Z': [[4, 5, 9, 10], [2, 6, 5, 9]],
+        'S': [[6, 7, 9, 10], [1, 5, 6, 10]],
+        'L': [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
+        'J': [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
+        'T': [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
+        'O': [[1, 2, 5, 6]]
+    }
+    SHAPES = ['I', 'Z', 'S', 'L', 'J', 'T', 'O']
+
     # constrructor
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.choice(self.SHAPES)
+        self.shape = self.VERSIONS[self.type]
+        self.color = random.randint(1,4)
+        self.orientation = 0
 
-    # image 
+    # image - version of the shape based on current orientation
+    def image(self):
+        return self.shape[self.orientation]
 
-    # Rotate
+    # Rotate the shape if possible
+    def rotate(self):
+        self.orientation = (self.orientation + 1) % len(self.shape)
 
 
 
